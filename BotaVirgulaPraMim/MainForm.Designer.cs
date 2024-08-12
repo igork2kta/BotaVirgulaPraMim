c7f0cb5505608a57ae52fe7197a9f34e35385a35
Igor Silva <igor.silva@energisa.com.br>
2024-08-12 16:36:51 -0300

    Salvando arquivos faltantes

--- BotaVirgulaPraMim/MainForm.Designer.cs
+++ BotaVirgulaPraMim/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BotaVirgulaPraMim.net_3._1
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             tb_dados = new System.Windows.Forms.TextBox();
             label1 = new System.Windows.Forms.Label();
             tb_commit = new System.Windows.Forms.TextBox();
--- BotaVirgulaPraMim/Program.cs
+++ BotaVirgulaPraMim/Program.cs
@@ -15,7 +15,7 @@ namespace BotaVirgulaPraMim.net_3._1
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
